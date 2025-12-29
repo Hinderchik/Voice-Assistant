9ef98266dd0ccfdeb57951be7c200558e66e5933
Hinderchik <gondonloxlp@gmail.com>
2025-12-29 13:50:07 +0300

    Fix syntax error in network.js

--- network.js
+++ network.js
@@ -35,7 +35,7 @@ class Network {
     sendMove(move) {
         fetch('/api/move', {
             method: 'POST',
-            headers: {'Content-Type': 'application/json'},
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(move)
         });
     }
